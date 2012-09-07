722af6e4897867519175cef7c2da1cd80f273a1f
Zac Stewart <zgstewart@gmail.com>
2012-09-06 23:07:51 -0400

    At first working ensemble model

--- controller.py
+++ controller.py
@@ -1,24 +1,70 @@
 import numpy as np
 from datasets import *
 from sklearn.neighbors import *
+from sklearn.linear_model import *
+from sklearn.svm import SVR
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.gaussian_process import GaussianProcess
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
 if __name__ == '__main__':
-  for dataset in range(1, 16):
-    print "Loading file..."
-    training_matrix, training_header = readFile("data/TrainingSet/ACT%(dataset)i_competition_training.csv" % {'dataset': dataset})
-    print type(training_matrix)
-    np.random.shuffle(training_matrix)
+  suffix = ''
+  submission = np.array(['MOLECULE','Prediction'], dtype=str)
+  for dataset in range(1, 2):
+    construct_ids, construct_X, construct_y, \
+        cv_ids, cv_X, cv_y = \
+        loadTrainingSet('data/TrainingSet/ACT%(dataset)i_competition_training%(suffix)s.csv' % {'dataset': dataset, 'suffix': suffix})
+    test_ids, test_X = \
+        loadTestSet('data/TestSet/ACT%(dataset)i_competition_test%(suffix)s.csv' % {'dataset': dataset, 'suffix': suffix})
+    models = [
+      Ridge(alpha=.5),
+      LinearRegression(),
+      SVR(),
+      NearestCentroid(),
+      GaussianProcess(),
+      DecisionTreeRegressor(),
+      RandomForestRegressor(),
+      GradientBoostingRegressor()
+    ]
 
-    construct, cv = training_matrix[:80,:], training_matrix[80:,:]
+    super_training_matrix = np.zeros((len(cv_y), len(models) + 1))
+    super_training_matrix[:,0] = cv_y
+    super_test_matrix = np.zeros((len(test_X), len(models)))
 
-    construct_ids = construct[:, 0]
-    construct_y   = construct[:, 1].astype(float)
-    construct_X   = construct[:, 2:].astype(int)
-    cv_ids = cv[:, 0]
-    cv_y   = cv[:, 1].astype(float)
-    cv_X   = cv[:, 2:].astype(int)
+    for (i, model) in enumerate(models):
+      print 'Training %(model)s...' % {'model': str(model).split('(')[0]}
+      model.fit(construct_X, construct_y)
+      print 'Predicting CV...'
+      preds = model.predict(cv_X).flatten()
+      print 'Adding predictions...'
+      super_training_matrix[:, i+1] = preds
+      print 'Predicting test set...'
+      test_preds = model.predict(test_X).flatten()
+      super_test_matrix[:, i] = test_preds
 
-    print 'Training...'
-    model = KNeighborsRegressor()
-    model.fit(construct_X, construct_y)
-    print 'Score: ' + str(model.score(cv_X, cv_y))
+    construct_frac = int(.8 * len(super_training_matrix))
+    super_construct, super_cv = super_training_matrix[:construct_frac,:], super_training_matrix[construct_frac:,:]
+
+    super_construct_y = super_construct[:, 0]
+    super_construct_X = super_construct[:, 1:]
+    super_cv_y = super_cv[:, 0]
+    super_cv_X = super_cv[:, 1:]
+
+    best_score = 0.
+    best_model = None
+    for (i, model) in enumerate(models):
+      print 'Super training %(model)s...' % {'model': str(model).split('(')[0]}
+      model.fit(super_construct_X, super_construct_y)
+      score = model.score(super_cv_X, super_cv_y)
+      print 'Score: ' + str(score)
+      if score > best_score:
+        best_score = score
+        best_model = model
+
+    print 'Best model: ' + str(best_model)
+    super_test_preds = best_model.predict(super_test_matrix)
+    set_predictions = np.array((test_ids, test_preds)).transpose()
+    submission = np.vstack((submission, set_predictions))
+    np.savetxt('submission.csv', submission, delimiter=',', fmt='%s')
--- datasets.py
+++ datasets.py
@@ -1,5 +1,8 @@
 import csv
 import numpy as np
+import os
+import pickle as pkl
+import string
 
 def readFile(filename, delimiter=','):
   '''Returns a data matrix and a header list'''
@@ -8,3 +11,71 @@ def readFile(filename, delimiter=','):
   f.close()
   matrix = np.loadtxt(filename, dtype=str, skiprows=1, delimiter=delimiter)
   return (matrix, header)
+
+def loadTestSet(filename):
+  cache_dir = os.path.join(os.getcwd(), 'data', 'cache', string.replace(filename.split('/')[-1], '.csv', ''))
+  test_matrix, test_header = readFile(filename)
+  test_ids = test_matrix[:, 0]
+  test_X = test_matrix[:, 1:].astype(int)
+  return (test_ids, test_X)
+
+def loadTrainingSet(filename):
+  cache_dir = os.path.join(os.getcwd(), 'data', 'cache', string.replace(filename.split('/')[-1], '.csv', ''))
+  if os.path.isdir(cache_dir):
+    print 'Loading pickled caches...'
+    f = open(os.path.join(cache_dir, 'construct_ids.pkl'), 'rb')
+    construct_ids = pkl.load(f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'construct_X.pkl'), 'rb')
+    construct_X = pkl.load(f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'construct_y.pkl'), 'rb')
+    construct_y = pkl.load(f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_ids.pkl'), 'rb')
+    cv_ids = pkl.load(f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_X.pkl'), 'rb')
+    cv_X = pkl.load(f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_y.pkl'), 'rb')
+    cv_y = pkl.load(f)
+    f.close()
+  else:
+    print 'Loading file...'
+    training_matrix, training_header = readFile(filename)
+
+    print 'Splitting CV...'
+    np.random.shuffle(training_matrix)
+    construct_frac = int(.8 * len(training_matrix))
+    construct, cv = training_matrix[construct_frac:,:], training_matrix[:construct_frac,:]
+
+    print 'Separating X/y, casting types...'
+    construct_ids = construct[:, 0]
+    construct_y   = construct[:, 1].astype(float)
+    construct_X   = construct[:, 2:].astype(int)
+    cv_ids = cv[:, 0]
+    cv_y   = cv[:, 1].astype(float)
+    cv_X   = cv[:, 2:].astype(int)
+
+    print 'Dumping parts to Pickle...'
+    os.makedirs(cache_dir)
+    f = open(os.path.join(cache_dir, 'construct_ids.pkl'), 'wb')
+    pkl.dump(construct_ids, f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'construct_X.pkl'), 'wb')
+    pkl.dump(construct_X, f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'construct_y.pkl'), 'wb')
+    pkl.dump(construct_y, f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_ids.pkl'), 'wb')
+    pkl.dump(cv_ids, f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_X.pkl'), 'wb')
+    pkl.dump(cv_X, f)
+    f.close()
+    f = open(os.path.join(cache_dir, 'cv_y.pkl'), 'wb')
+    pkl.dump(cv_y, f)
+    f.close()
+  return (construct_ids, construct_X, construct_y, cv_ids, cv_X, cv_y)
